980b9d9656fde6c6a66f56ca42ade15cc9cf477c
LudovicoRighi <ludo.righi@hotmail.it>
2020-12-11 11:25:27 +0100

    Project

---
+++ EasyReview/EasyReviewEJB/ejbModule/examplePackage/ProvaService.java
@@ -0,0 +1,13 @@
+package examplePackage;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class ProvaService {
+	@EJB(name = "examplePackage/ProvaService")
+	 public String sayHello(String name) {
+	        return "ciaooo, "  + name;
+	    }
+}
